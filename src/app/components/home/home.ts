6d858f7918387b35575de1e935f7a6e228e1fec4
vinivargasr <166772508+vinivargasr@users.noreply.github.com>
2026-01-24 22:10:51 -0300

    animaçãotexto

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,11 +1,48 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import gsap from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {
+export class Home implements AfterViewInit {
 
+  ngAfterViewInit(): void {
+
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: '.home',
+        start: 'top top',
+        end: '+=100%',
+        scrub: true,
+        pin: true,
+        anticipatePin: 1
+        // markers: true
+      }
+    });
+
+    // Fundo — zoom cinematográfico
+    tl.to('.home-fundo', {
+      scale: 1.08,
+      ease: 'none'
+    }, 0);
+
+    // PNG — zoom levemente maior
+    tl.to('.home-png', {
+      scale: 1.1,
+      ease: 'none'
+    }, 0);
+
+    // Texto sai suavemente
+    tl.to('.home-txt', {
+      opacity: 0,
+      y: -40,
+      ease: 'none'
+    }, 0);
+  }
 }
